599fc3fe4778961e30d68070d131fe1463efa432
Rivares <rivares@protonmail.com>
2023-12-12 01:04:00 +0300

    Two variants of decide

--- main.cpp
+++ main.cpp
@@ -4,32 +4,10 @@
 
 #include <unordered_set>
 #include <string>
-
-struct Node
-{
-    Node(uint64_t i_, uint64_t j_, uint64_t value_) :
-        i(i_), j(j_), value(value_)
-    {}
-
-    uint64_t i;
-    uint64_t j;
-    uint64_t value;
-};
-
-bool operator == (const Node& lNode, const Node& rNode)
-{
-    return ( (lNode.i == rNode.i) && (lNode.j == rNode.j) );
-}
-
-struct NodeHash
-{
-    size_t operator () (const Node& node) const
-    {
-        return std::hash<size_t>{}(node.i) + std::hash<size_t>{}(node.j);
-    }
-};
+#include <tuple>
 
 
+struct NodeHash;
 
 
 // Lazy initialization + Proxy
@@ -39,110 +17,156 @@ class Matrix
 
 public:
 
-    class RowProxy
-    {
-        T m_i;
-        T m_j;
-        T m_val;
-
-        Matrix* m_parent = nullptr;
-
-    public:
-
-        RowProxy() = default;
-
-        RowProxy(Matrix* ptr_, const T& i_) : m_parent(ptr_), m_i(i_)
-        {   std::cout << __PRETTY_FUNCTION__ << '\n';   }
-
-        ~RowProxy()
-        {   std::cout << __PRETTY_FUNCTION__ << '\n';   }
-
-        T& operator [](T j_)
-        {
-            std::cout << __PRETTY_FUNCTION__ << '\n';
-
-            if (j_ >= 0)
-            {
-                m_j = j_;
-            }
-
-            return m_parent->getDefVal();
-        }
-
-        void setVal(const T& val_)
-        {   m_val = val_;   }
-
-        const T& getVal() const
-        {   return m_val;   }
-    };
-
     Matrix(): m_defValue(value)
     {   std::cout << __PRETTY_FUNCTION__ << '\n';   }
     ~Matrix()
     {   std::cout << __PRETTY_FUNCTION__ << '\n';   }
 
-    RowProxy& operator [](T i_)
+
+    struct Node
+    {
+        explicit Node(T i_ = 0, T j_ = 0, T value_ = 0) :
+            m_i(i_), m_j(j_), m_value(value_), m_parent(nullptr)
+        {}
+
+        explicit Node(Matrix* parent_, T value_, T i_ = 0, T j_ = 0) :
+            m_parent(parent_)
+          , m_value(value_)
+          , m_i(i_)
+          , m_j(j_)
+        {   std::cout << __PRETTY_FUNCTION__ << '\n';   }
+
+        ~Node()
+        {   std::cout << __PRETTY_FUNCTION__ << '\n';   }
+
+
+        Node& operator [] (T j_) // optional
+        {
+            std::cout << __PRETTY_FUNCTION__ << '\n';
+
+            if (j_ >= 0)
+            {   m_j = j_;   }
+
+            return (*this);
+        }
+
+        void operator = (const T& val_)
+        {
+            std::cout << __PRETTY_FUNCTION__ << ' ' << val_ << '\n';
+
+            if (val_ != m_parent->m_defValue)
+            {
+                this->m_value = val_;
+                m_parent->m_data.insert(*this);
+                m_parent->m_data1.insert({this->m_i, this->m_j, this->m_value, this->m_parent});
+            }
+            else
+            {
+                m_parent->m_data.erase(*this);
+                m_parent->m_data1.erase({this->m_i, this->m_j, this->m_value, this->m_parent});
+            }
+        }
+
+        bool operator == (const T& rVal_) const
+        {
+            std::cout << __PRETTY_FUNCTION__ << '\n';
+
+            Node findingNode {m_i, m_j, m_value};
+
+//            if (auto it = m_parent->m_data.find(findingNode); it != m_parent->m_data.end())
+//            {   return ((*it).m_value == rVal_);  }
+            if (auto it = m_parent->m_data1.find({m_i, m_j, m_value, m_parent}); it != m_parent->m_data1.end())
+            {   return (std::get<2>((*it)) == rVal_);  }
+
+            return false;
+        }
+
+        bool operator != (const T& rVal_) const
+        {
+            std::cout << __PRETTY_FUNCTION__ << '\n';
+
+            return !((*this) == rVal_);
+        }
+
+
+        T m_i;
+        T m_j;
+        T m_value;
+
+        Matrix* m_parent;
+    };
+
+    struct NodeHash
+    {
+        size_t operator () (const Node& node) const
+        {
+            return std::hash<size_t>{}(node.m_i) + std::hash<size_t>{}(node.m_j);
+        }
+    };
+
+    struct NodeEqual
+    {
+        bool operator () (const Node& lNode, const Node& rNode) const
+        {
+            return ( (lNode.m_i == rNode.m_i) && (lNode.m_j == rNode.m_j) );
+        }
+    };
+
+    struct Node1Hash
+    {
+        size_t operator () (const std::tuple<T, T, T, Matrix*>& node) const
+        {
+            return std::hash<size_t>{}(std::get<0>(node)) + std::hash<size_t>{}(std::get<1>(node));
+        }
+    };
+
+    struct Node1Equal
+    {
+        bool operator () (const std::tuple<T, T, T, Matrix*>& lNode, const std::tuple<T, T, T, Matrix*>& rNode) const
+        {
+            return ( (std::get<0>(lNode) == std::get<0>(rNode)) && (std::get<1>(lNode) == std::get<1>(rNode)) );
+        }
+    };
+
+    Node operator [] (T i_)
     {
         std::cout << __PRETTY_FUNCTION__ << '\n';
 
         if (i_ >= 0)
         {
-            RowProxy* obj = new RowProxy(this, i_);
-//            m_data.insert(obj);
-
-            return *obj;
+            return Node(this, m_defValue, i_);
         }
-        RowProxy dumy;
-        return dumy;
+
+        return Node();
     }
 
-    T& getDefVal()
-    {   return m_defValue;   }
+    // TODO Impl iterators
+    auto begin()
+    {   return m_data1.begin();  }
 
-    Matrix& operator = (const T& val_)
-    {
-        std::cout << __PRETTY_FUNCTION__ << ' ' << '\n';
-        m_data.back()->setVal(val_);
+    auto cbegin() const
+    {   return m_data1.cbegin();  }
 
-        return *this;
-    }
+    auto end()
+    {   return m_data1.end();  }
 
-    bool operator == (const T& rVal_) const
-    {
-        std::cout << __PRETTY_FUNCTION__ << ' ' << '\n';
+    auto cend() const
+    {   return m_data1.cend();  }
 
-        return true;//(m_data.back()->getVal() == rVal_);
-    }
-    bool operator == (const Matrix& rVal_)
-    {
-        std::cout << __PRETTY_FUNCTION__ << ' ' << '\n';
 
-        return (m_data.back()->getVal() == rVal_.getData().back()->getVal());
-    }
-    bool operator < (const Matrix& rVal_)
-    {
-        std::cout << __PRETTY_FUNCTION__ << ' ' << '\n';
-
-        return (m_data.back()->getVal()  < rVal_.getData().back()->getVal());
-    }
-    bool operator <= (const Matrix& rVal_)
-    {
-        std::cout << __PRETTY_FUNCTION__ << ' ' << '\n';
-
-        return (m_data.back()->getVal() <= rVal_.getData().back()->getVal());
-    }
-
-    std::unordered_set<RowProxy, std::hash<RowProxy>> getData() const
-    {   return m_data; }
 
     uint64_t size() const
-    {   return m_data.size(); }
+    {   return m_data1.size(); }
 
 private:
     T m_defValue;
 
-    std::unordered_set<RowProxy, NodeHash> m_data;
+    std::unordered_set<Node, NodeHash, NodeEqual> m_data;
+    std::unordered_set<std::tuple<T, T, T, Matrix*>, Node1Hash, Node1Equal> m_data1;
 };
+
+
+
 
 
 #include <cassert>
@@ -177,28 +201,41 @@ int main()
         // бесконечная матрица int заполнена значениями -1
         Matrix<int, -1> matrix;
 
+
+        std::cout << "\nWork part:\n\n";
+
         assert(matrix.size() == 0); // все ячейки свободны
 
         matrix[1][2] = 8;
+        matrix[1][2] = 8;
+
+        assert(matrix.size() == 1);
+        assert(matrix[1][2] == 8);
+
         auto a = matrix[1][2];
 
-//        assert(a == 0);
-//        assert(a == -1);
+        assert(a == 8);
+        assert(a != -1);
+        assert(matrix.size() == 1);
+
+        matrix[100][100] = 314;
+        assert(matrix[100][100] == 314);
         assert(matrix.size() == 2);
 
-//        matrix[100][100] = 314;
-//        assert(matrix[100][100] == 314);
-//        assert(matrix.size() == 1);
+
 //        // выведется одна строка
 //        // 100100314
-//        for(auto c: matrix)
-//        {
-//        int x;
-//        int y;
-//        int v;
-//        std::tie(x, y, v) = c;
-//        std::cout << x << y << v << std::endl;
-//        }
+        for(auto item: matrix)
+        {
+            int x;
+            int y;
+            int v;
+            Matrix<int, -1>* vv;
+            std::tie(x, y, v, vv) = item;
+            std::cout << x << y << v << std::endl;
+//            auto [xx, yy, vv, ww] = item;
+//            std::cout << xx << yy << vv << std::endl;
+        }
 
         std::cout << "\n\nDestructions objects:\n";
     }
@@ -209,4 +246,5 @@ int main()
     ProfilerStop();
     return 0;
 }
+
 
